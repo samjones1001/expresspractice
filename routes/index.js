cfa67a0169ab3f1de1037f2d435dda425d9e4a8c
Sam Jones <sam.jones494@googlemail.com>
2017-03-15 11:20:32 +0000

    set up node server

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'MyNodeTest' });
+});
+
+router.post('/', function (req, res) {
+  res.send('Got a POST request');
+});
+
+module.exports = router;
